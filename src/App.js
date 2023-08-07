eaf5d8e8c2fb728e5881bc2305f298c2f778b836
Alvin-Karanja <alvin2@proton.me>
2023-08-07 17:27:59 +0300

    creating class components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Card extends React.Component {
+    render() {
+        return (
+            <div className="github-profile">
+                <img src="https://placehold.it/75" />
+                <div className="info">
+                    <div className="name">Name here</div>
+                    <div className="company">Company here..</div>
+                </div>
+            </div>
+        );
+    }
 }
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="header">The Github Cards App</div>
+                <Card />
+            </div>
+        );
+    }
+}
+
+const mountnode = document.getElementById('root');
+ReactDOM.render(<App />, mountnode);
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
