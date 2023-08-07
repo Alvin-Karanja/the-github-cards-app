c944efff2fc9c1c34c7d5af6556cd2f4149a722a
Alvin-Karanja <alvin2@proton.me>
2023-08-07 17:53:56 +0300

    working with data

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+const CardList = (props) => (
+    <div>
+        {testData.map(profile => <Card {...profile}/>)}
+    </div>
+);
+
 class Card extends React.Component {
     render() {
+        const profile = this.props;
         return (
             <div className="github-profile">
-                <img src="https://placehold.it/75" />
+                <img src={profile.avatar_url}  alt="profile avatar/image"/>
                 <div className="info">
-                    <div className="name">Name here</div>
-                    <div className="company">Company here..</div>
+                    <div className="name">{profile.name}</div>
+                    <div className="company">{profile.company}</div>
                 </div>
             </div>
         );
@@ -22,7 +35,7 @@ class App extends React.Component {
         return (
             <div>
                 <div className="header">The Github Cards App</div>
-                <Card />
+                <CardList />
             </div>
         );
     }
